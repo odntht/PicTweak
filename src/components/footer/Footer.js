4ae817afad269d2c493c97adb8818ae08e43bd98
Odntht <odntht@gmail.com>
2021-06-03 18:54:31 -0300

    migrated footer to styled components

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,24 @@
+import styled from 'styled-components';
 import './Footer.css';
+
+const FooterWrapper = styled.footer`
+  margin: auto;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  a {
+    text-decoration: none;
+    color: black;
+  }
+`;
 
 // Add link para meu LinkedIn
 function Footer() {
   return (
-    <footer>
+    <FooterWrapper>
       <a href="https://www.linkedin.com/in/odntht/?locale=en_US">
         By Fernando Cavalcante
       </a>
-    </footer>
+    </FooterWrapper>
   );
 }
 
